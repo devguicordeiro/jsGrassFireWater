5b35a11889b4ae44aaa8afb0f6cafe05b23a455d
Guilherme Cordeiro <devguicordeiro@gmail.com>
2023-02-21 20:41:13 -0300

    small update

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
 const choices = ["grass", "water", "fire"];
 let winners = [];
 
-function game() {
-  playRound();
+function restartGame()
 
-  logWins();
+function game() {
+  let imgs = documento.querySelectorAll("img")
+  imgs.forEach((img) => img.addEventListener(("click"), () => {
+    if(img.id){
+      playRound(img.id)
+    }
+  } ))
 }
 
-function playRound(round) {
+function playRound(playerChoice) {
+
+  let wins = roundWinsCheck()
+    if(wins >= 5){
+      return
+    }
+
   const botSelection = botChoice();
 
   const winner = winnerCheck(playerSelection, botSelection);
 
   winners.push(winner);
+
+  tallyWins();
+}
+
+function tallyWins() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let botWins = winners.filter((item) => item == "Bot").length;
+  document.querySelector(".playerScore").textContent = `Score: ${playerWins}`;
+  document.querySelector(".botScore").textContent = `Score: ${botWins}`;
 }
 
 function botChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function roundWinsCheck() {
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let botWins = winners.filter((item) => item == "Bot").length;
+  return Math.max(playerWins, botWins)
 }
 
 function winnerCheck(choiceP, choiceB) {
@@ -38,3 +64,5 @@ function logWins() {
   let botWins = winners.filter((item) => item == "Bot").length;
   let ties = winners.filter((item) => item == "Tie").length;
 }
+
+game();
